b27ab070318598e239f22210eb407aff0f37a1aa
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-07-11 00:09:07 -0500

    feat: :sparkles: Se completo el punto 4
    Se completo el punto 4 pudiendo mostrar los datos en orden alfabetico
    
    TODO : /** Reorganizar el SQL **/

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from 'dotenv';
+import cors from 'cors'
 import path from 'path'
 import bodegas from './routes/bodegas.routes.js'
 
@@ -10,13 +11,16 @@ const app = express();
 //setting
 dotenv.config();
 app.set("port", process.env.PORT || 3000);
+const __dirname = path.dirname(import.meta.url);
+
 
 
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-front= path.join(__dirname + '/public')
+const front= path.join(__dirname + '/public')
 app.use(express.static(front)); 
+app.use(cors({ origin: '*'}));
 
 //Routes
 app.use('/bodegas', bodegas);
